3d3db0560b4f3026756e8c7e9a41f2dcce8ad36b
mtuaillo <2207367+mtuaillo@users.noreply.github.com>
2025-04-14 14:52:53 +0200

    Exemples Doctrine

---
+++ src/Services/DoctrineBestPractices.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+class Event
+{
+    public function __construct(
+        private \DateTime $startsAt,
+        private string $duration,
+    ) {
+    }
+
+    public function getStartsAt(): \DateTime
+    {
+        return $this->startsAt;
+    }
+
+    public function getDuration(): string
+    {
+        return $this->duration;
+    }
+}
+
+class ImmutableDateExample
+{
+    public function dateTimeMustBeImmutable(): void
+    {
+        $event = new Event(new \DateTime('2025-04-14 14:00:00'), 'PT3H');
+
+        $endsAt = $event->getStartsAt()->add(new \DateInterval($event->getDuration()));
+        dump($endsAt); // 2025-04-14 17:00:00.0 UTC (+00:00)
+        dump($event->getStartsAt()); // 2025-04-14 17:00:00.0 UTC (+00:00)
+    }
+
+    public function floatPrecision(): void
+    {
+        $a = 0.1 + 0.2;
+        dump($a); // 0.3
+        dump($a == 0.3); // false
+        dump(number_format($a, 20)); // "0.30000000000000004441"
+    }
+}
